cfb4aa6723880573c7519d7ca63f1cbf3af6a617
Qing Lin <77032312+Lin-Tsing@users.noreply.github.com>
2024-09-11 14:02:26 +0200

    Update classification.sh

--- classification.sh
+++ classification.sh
@@ -22,8 +22,6 @@ input_y_file=${11}
 input_X_test_file=${12}
 # input_y_test_file=${13}
 
-
-
 echo ${feature_method} ${feature_number} ${classif_method} ${classic_labels} running!
 
 work_path=/mnt/export/home/nsccwuxi_scau/zhangz2/data/USER/linqing/identification
